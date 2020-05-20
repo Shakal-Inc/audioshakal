abad4ddfd704ebca0f144533a950ae71eee2c3b5
Danil <albutovdanil@gmail.com>
2020-05-20 22:22:22 +0400

    add general buttons; add request permission

--- app/src/main/java/com/ulstu/retrobitconverter/MainActivity.java
+++ app/src/main/java/com/ulstu/retrobitconverter/MainActivity.java
@@ -1,12 +1,37 @@
 package com.ulstu.retrobitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("myTag","Permission is granted");
+                return true;
+            } else {
+
+                Log.v("myTag","Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("myTag","Permission is granted");
+            return true;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnOpenFileClick(View view) {
+        OpenFileDialog fileDialog = new OpenFileDialog(this);
+        if(isStoragePermissionGranted()) {
+            fileDialog.show();
+        }
 
     }
 }
--- app/src/main/java/com/ulstu/retrobitconverter/OpenFileDialog.java
+++ app/src/main/java/com/ulstu/retrobitconverter/OpenFileDialog.java
@@ -1,4 +1,41 @@
 package com.ulstu.retrobitconverter;
 
-public class OpenFileDialog {
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+
+public class OpenFileDialog extends AlertDialog.Builder {
+
+    private String currentPath = Environment.getExternalStorageDirectory().getPath();
+
+
+    private String[] getFiles(String directoryPath){
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        String[] result = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            result[i] = files[i].getName();
+        }
+        return result;
+    }
+
+    public OpenFileDialog(Context context) {
+        super(context);
+        try {
+            setPositiveButton(android.R.string.ok, null)
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setItems(getFiles(currentPath), null);
+        } catch (Exception e) {
+            Log.d("myTag", e + "");
+        }
+    }
+
 }
