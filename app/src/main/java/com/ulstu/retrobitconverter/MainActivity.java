0dc66ddd82b43227eb56907a68551c007e155d82
Danil <albutovdanil@gmail.com>
2020-05-21 18:40:29 +0400

    add background for MainActivity and checker device orientation to display the background correctly

--- app/src/main/java/com/ulstu/retrobitconverter/MainActivity.java
+++ app/src/main/java/com/ulstu/retrobitconverter/MainActivity.java
@@ -6,13 +6,43 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  int  orientation;
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+
+        orientation = newConfig.orientation;
+        setBackgroundImage(newConfig.orientation);
+    }
+
+    private void setBackgroundImage(final int orientation)
+    {
+        LinearLayout layout;
+        layout = (LinearLayout) findViewById(R.id.image_layout);
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+            layout.setBackgroundResource(R.drawable.background_app);
+        else if (orientation == Configuration.ORIENTATION_PORTRAIT)
+            layout.setBackgroundResource(R.drawable.background_portrain_app);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setBackgroundImage(orientation);
+    }
 
     public  boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -36,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        orientation  = Configuration.ORIENTATION_PORTRAIT;
     }
 
     public void OnOpenFileClick(View view) {
