0ad8d974b623fc1c2a843fdd4c58a8c0ec35a970
Danil <albutovdanil@gmail.com>
2020-05-22 19:58:56 +0400

    Upload project

---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class FileManager : MonoBehaviour
+{
+    string path;
+    public RawImage image;
+
+    public void OpenExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("Hello World", "", "jpg");
+    }
+
+    void GetImg()
+    {
+        if (path != null)
+        {
+            UpdateImg();
+        }
+    }
+
+    void UpdateImg()
+    {
+        WWW www = new WWW("file:///" + path);
+        image.texture = www.texture;
+    }
+}
